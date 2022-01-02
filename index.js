864b37786557dd8814736c68345e0d86956ae8fe
rahat1999 <rahat.is.1999@gmail.com>
2022-01-02 23:01:16 +0600

    'checking_commit'

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const cors = require('cors')
+const ObjectId = require('mongodb').ObjectId;
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000;
+
+/*========= Middleware============== */
+app.use(cors())
+app.use(express.json())
+
+/* ===========MongoDb================ */
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2rvjh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("insertDB");
+        const haiku = database.collection("haiku");
+
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to EDUcamp!')
+})
+
+app.listen(port, () => {
+    console.log(`listening at :${port}`)
+})
