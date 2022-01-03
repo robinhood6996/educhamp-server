7e3a77c913d51e9414d55e73ef148e2128df1679
Fahad98723 <md.ravi63@gmail.com>
2022-01-04 00:48:42 +0600

    stripe

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express')
 const cors = require('cors')
 const ObjectId = require('mongodb').ObjectId;
 const { MongoClient } = require('mongodb');
+const res = require('express/lib/response');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 /*========= Middleware============== */
 app.use(cors())
 app.use(express.json())
@@ -18,16 +19,90 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const database = client.db("insertDB");
-        const haiku = database.collection("haiku");
+        const database = client.db("EDU_Champ");
         const reviewCollection = database.collection("review")
+        const usersCollection = database.collection("users");
 
 
+        /* =========User data Post api for save user email,name,photo, in db=== */
+        app.post('/users', async (req, res) => {
+            const result = await usersCollection.insertOne(req.body)
+            // res.send(result)
+            console.log(result);
+        })
+        /* ===========upsert/put new user Api for google log in data=========== */
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user }
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            // console.log(result);
+            res.send(result)
+        })
+
+        app.get('/users', async(req, res) => {
+            const result = await usersCollection.find().toArray()
+            res.send(result)
+        })
+
+        //make user admin
+        app.put('/users/admin', async (req, res) => {
+            const email = req.body.email 
+            const filter = {email : email}
+            const updateDoc = {
+                $set : {
+                    role : 'admin'
+                }
+            }
+            const user = await usersCollection.updateOne(filter, updateDoc)
+            res.json(user)
+        })
+
+        app.get('/users/:email', async(req, res) => {
+            const email = req.params.email
+            const query = {email : email}
+            const user = await usersCollection.findOne(query)
+            let isAdmin = false
+            if (user.role === 'admin') {
+                isAdmin = true
+            }
+            else{
+                isAdmin = false
+            }
+            const result = {admin : isAdmin}
+            res.send(result)
+        })
+
+        //review collection for user
         app.post('/review', async (req, res) => {
             const data = req.body
             const review = await reviewCollection.insertOne(data)
             res.json(review)
         })
+
+        app.get('/review', async(req, res) => {
+            const review = await reviewCollection.find({}).toArray()
+            res.send(review)
+        })
+
+        app.post("/create-payment-intent", async (req, res) => {
+            const paymentInfo = req.body;
+            const amount = paymentInfo.totalAmount * 100
+            // Create a PaymentIntent with the order amount and currency
+            console.log(amount);
+            const paymentIntent = await stripe.paymentIntents.create({
+              amount: amount,
+              currency: "eur",
+              payment_method_types: ["card"],
+            });
+          
+            res.send({
+              clientSecret: paymentIntent.client_secret,
+            });
+          });
+
+
     } finally {
         //   await client.close();
     }
